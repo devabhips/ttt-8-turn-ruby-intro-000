6d1652038e23da94130691be8099d612e2653156
dazzling-acrobat-7297 <abhips@gmail.com>
2020-02-10 20:44:52 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+def display_board(board)
+  puts "\n------------\n #{board[0]} | #{board[1]} | #{board[2]} \n---------\n #{board[3]} | #{board[4]} | #{board[5]} \n---------\n #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  user_input = input.strip.to_i - 1
+end
+
+def valid_move?(board, index)
+  return false if index < 0 || index > 8
+
+  return false if board[index] == 'X' || board[index] == 'O'
+
+  # return true if board[index] == '   ' || board[index] == '' || board[index].nil?
+
+  true
+end
+
+def move(board, index, position_character='X')
+  if valid_move?(board, index)
+    board[index] = position_character
+  end
+end
+
+def turn
+  puts 'Please enter 1-9:'
+  input = gets.strip.to_i
+end
